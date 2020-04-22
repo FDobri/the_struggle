fda631a409ab83843105909ec0ef65183a8f8fd8
DESKTOP-KE05CFV\User <filip_dobrinic@bitbucket.org>
2020-04-22 20:42:51 +0200

    Added basic character movement (including jumping) and basic scene platform elements.

---
+++ Assets/Scripts/PlayerGroundChecker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerGroundChecker : MonoBehaviour
+{
+	public bool IsGrounded{ get; private set; }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.collider.tag.Equals("Ground"))
+		{
+			IsGrounded = true;
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.transform.tag.Equals("Ground"))
+		{
+			IsGrounded = false;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+	public float speedMultiplier;
+	public float jumpForce;
+
+	[Tooltip("Should be Impulse for now.")]
+	public ForceMode2D jumpForceMode;
+
+	private PlayerGroundChecker _groundChecker;
+	private Rigidbody2D _rigidbody;
+	private Vector2 _direction;
+	private Vector2 _jumpVector;
+
+	private const string HORIZONTAL_AXIS = "Horizontal";
+
+	private void Start()
+	{
+		_jumpVector = new Vector2(0f, jumpForce);
+		_groundChecker = transform.GetComponentInChildren<PlayerGroundChecker>();
+		_rigidbody = transform.GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) && _groundChecker.IsGrounded)
+		{
+			_rigidbody.AddForce(_jumpVector, jumpForceMode);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		float direction = Input.GetAxis(HORIZONTAL_AXIS);
+		transform.position += new Vector3(direction * Time.fixedDeltaTime * speedMultiplier, 0f);
+	}
+}
