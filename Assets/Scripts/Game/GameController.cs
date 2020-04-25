5e0fd50f161fd5607cf793abc427c7fb91220500
Marco Krevatin <marco.krevatin@playrix.hr>
2020-04-25 16:01:38 +0200

    Added GameController and CharacterSelect stuff.

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CharacterType
+{
+	Male,
+	Female
+}
+
+public class GameController : MonoBehaviour
+{
+	public static GameController gameControllerInstance;
+
+	[HideInInspector]
+	public CharacterType character;
+
+	// Start is called before the first frame update
+	void Awake()
+    {
+		if (gameControllerInstance == null)
+		{
+			gameControllerInstance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player/LoadPlayerAssets.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadPlayerAssets : MonoBehaviour
+{
+    public Sprite femaleCharacter;
+    public Sprite maleCharacter;
+    public SpriteRenderer spriteRenderer;
+
+    private GameController _gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameController = GameController.gameControllerInstance;
+
+        if (_gameController != null && _gameController.character.ToString() == "Female")
+        {
+            spriteRenderer.sprite = femaleCharacter;
+        }
+        else 
+        {
+            spriteRenderer.sprite = maleCharacter;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
