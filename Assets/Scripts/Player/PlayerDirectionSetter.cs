1d08a3989906fe448ad2ee45ce18b83a503d4817
mlogic1 <mlogic1@users.noreply.github.com>
2020-04-25 12:14:45 +0200

    added playerDirectionSetter and updated PlayerMovementController

---
+++ Assets/Scripts/Player/PlayerDirectionSetter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDirectionSetter : MonoBehaviour
+{
+    public float RotationSpeed = 20.0f;
+    public PlayerMovementController playerMovementController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetDirection = new Vector3(55.0f, 0.0f, 0.0f);
+        Direction _playerDirection = playerMovementController.GetPlayerDirection();
+
+        /*float singleStep = 20.0f * Time.deltaTime;
+        switch (_playerDirection)
+        {
+            case Direction.LEFT:
+                {
+                    Vector3 targetAngle = new Vector3(0f, 181f, 0f);
+
+                    Vector3 currentAngle = transform.eulerAngles;
+
+                    currentAngle = new Vector3(
+                         Mathf.LerpAngle(currentAngle.x, targetAngle.x, singleStep),
+                         Mathf.LerpAngle(-currentAngle.y, targetAngle.y, singleStep),
+                         Mathf.LerpAngle(currentAngle.z, targetAngle.z, singleStep));
+
+                    transform.eulerAngles = currentAngle;
+                    break;
+                }
+
+            case Direction.RIGHT:
+                {
+                    Vector3 targetAngle = new Vector3(0f, 0f, 0f);
+
+                    Vector3 currentAngle = transform.eulerAngles;
+
+                    currentAngle = new Vector3(
+                         Mathf.LerpAngle(currentAngle.x, targetAngle.x, singleStep),
+                         Mathf.LerpAngle(-currentAngle.y, targetAngle.y, singleStep),
+                         Mathf.LerpAngle(currentAngle.z, targetAngle.z, singleStep));
+
+                    transform.eulerAngles = currentAngle;
+                    break;
+                }
+
+            default:
+                break;
+
+        }
+        */
+    }
+}
--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,17 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public enum Direction
+{
+    NONE,
+    LEFT,
+    RIGHT
+};
 
 public class PlayerMovementController : MonoBehaviour
 {
-	public float speedMultiplier;
-	public float jumpForce;
+    
 
-	[Tooltip("Should be Impulse for now.")]
+    public float speedMultiplier;
+	public float jumpForce;
+    public GameObject _playerModel;
+
+    [Tooltip("Should be Impulse for now.")]
 	public ForceMode2D jumpForceMode;
 
 	private PlayerGroundChecker _groundChecker;
 	private Rigidbody2D _rigidbody;
 	private Vector2 _direction;
 	private Vector2 _jumpVector;
+    
+    private Direction _playerDirection = Direction.RIGHT;
 
 	private const string HORIZONTAL_AXIS = "Horizontal";
 
@@ -20,7 +33,13 @@ public class PlayerMovementController : MonoBehaviour
 		_jumpVector = new Vector2(0f, jumpForce);
 		_groundChecker = transform.GetComponentInChildren<PlayerGroundChecker>();
 		_rigidbody = transform.GetComponent<Rigidbody2D>();
-	}
+        Assert.IsNotNull(_playerModel, "Player model Game Object must be set in editor");
+    }
+
+    public Direction GetPlayerDirection()
+    {
+        return _playerDirection;
+    }
 
 	private void Update()
 	{
@@ -31,13 +50,24 @@ public class PlayerMovementController : MonoBehaviour
 
 		if (Input.GetKey(KeyCode.K))
 		{
-			FindObjectOfType<AudioManager>().PlaySound("ButtonClick");
-		}
-	}
+            //FindObjectOfType<AudioManager>().PlaySound("ButtonClick");
+        }
+
+    }
 
 	private void FixedUpdate()
 	{
 		float direction = Input.GetAxis(HORIZONTAL_AXIS);
+        
 		transform.position += new Vector3(direction * Time.fixedDeltaTime * speedMultiplier, 0f);
-	}
+
+        if (direction < 0.0f)
+        {
+            _playerDirection = Direction.LEFT;
+        }
+        else
+        {
+            _playerDirection = Direction.RIGHT;
+        }
+    }
 }
