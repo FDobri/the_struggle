59754467f47440d85ba460b41949aed51c43235a
mlogic1 <mlogic1@users.noreply.github.com>
2020-04-25 17:16:00 +0200

    updated vor dialogs, added scenes 2 to 5

--- Assets/Scripts/VOR/VorDialogs.cs
+++ Assets/Scripts/VOR/VorDialogs.cs
@@ -20,12 +20,23 @@ public class VORDialogs
     public List<VORText> Dialogs_Level_1 = new List<VORText>();
     public List<VORText> Dialogs_Level_2 = new List<VORText>();
     public List<VORText> Dialogs_Level_3 = new List<VORText>();
+    public List<VORText> Dialogs_Level_4 = new List<VORText>();
+    public List<VORText> Dialogs_Level_5 = new List<VORText>();
 
     public VORDialogs()
     {
         Dialogs_Level_1.Add(new VORText("Hello", 3.0f));
         Dialogs_Level_1.Add(new VORText("I am your Voice of Reason", 5.0f));
         Dialogs_Level_1.Add(new VORText("Play your adventure", 6.0f));
+
+        Dialogs_Level_2.Add(new VORText("Welcome to level 2", 3.0f));
+        Dialogs_Level_2.Add(new VORText("Continue your adventure", 5.0f));
+
+        Dialogs_Level_3.Add(new VORText("Level 3", 5.0f));
+
+        Dialogs_Level_4.Add(new VORText("Level 4", 5.0f));
+
+        Dialogs_Level_5.Add(new VORText("Level 5", 5.0f));
     }
 
     public List<VORText> GetDialogsForLevel(int level)
@@ -40,6 +51,12 @@ public class VORDialogs
 
             case 3:
                 return Dialogs_Level_3;
+
+            case 4:
+                return Dialogs_Level_4;
+
+            case 5:
+                return Dialogs_Level_5;
 
             default:
                 return null;
