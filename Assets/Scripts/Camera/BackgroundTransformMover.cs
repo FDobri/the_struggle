f4590e622bc0a1d806a83ea90d6736c5767361c7
Marco Krevatin <marco.krevatin@playrix.hr>
2020-04-26 00:49:58 +0200

    Added level 2 textures and background mover script.

--- Assets/Scripts/Camera/BackgroundTransformMover.cs
+++ Assets/Scripts/Camera/BackgroundTransformMover.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundImageMover : MonoBehaviour
+public class BackgroundTransformMover : MonoBehaviour
 {
     public float offset;
     public float speed;
@@ -12,8 +12,8 @@ public class BackgroundImageMover : MonoBehaviour
     private float _initialValueX;
     private float _maximumValueX;
     private float _minimumValueX;
-    private float _currentValueX; 
-    
+    private float _currentValueX;
+
     private float _initialValueY;
     private float _maximumValueY;
     private float _minimumValueY;
@@ -21,21 +21,21 @@ public class BackgroundImageMover : MonoBehaviour
 
     private bool _moveDirection = true;
 
-    private RectTransform imagePosition;
+    private Transform imagePosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        imagePosition = gameObject.GetComponent<RectTransform>();
-        
+        imagePosition = gameObject.GetComponent<Transform>();
+
         // HORIZONTAL MOVEMENT
-        _initialValueX = imagePosition.rect.x;
+        _initialValueX = imagePosition.localPosition.x;
         _maximumValueX = _initialValueX + offset;
         _minimumValueX = _initialValueX - offset;
         _currentValueX = _initialValueX;
 
         // VERZICAL MOVEMENT
-        _initialValueY = imagePosition.rect.y;
+        _initialValueY = imagePosition.localPosition.y;
         _maximumValueY = _initialValueY + offset;
         _minimumValueY = _initialValueY - offset;
         _currentValueY = _initialValueY;
@@ -64,7 +64,7 @@ public class BackgroundImageMover : MonoBehaviour
                 _currentValueY -= speed;
             }
 
-            imagePosition.localPosition = new Vector3(0, _initialValueY - _currentValueY);
+            imagePosition.localPosition = new Vector3(imagePosition.localPosition.x, _initialValueY - _currentValueY, imagePosition.localPosition.z);
         }
         else
         {
@@ -85,7 +85,7 @@ public class BackgroundImageMover : MonoBehaviour
                 _currentValueX -= speed;
             }
 
-            imagePosition.localPosition = new Vector3(_initialValueX - _currentValueX, 0);
+            imagePosition.localPosition = new Vector3(_initialValueX - _currentValueX, imagePosition.localPosition.y, imagePosition.localPosition.z);
         }
     }
 }
